0ebfbce7f1738c0e11836b11231f8948d9141c2d
houmo <houyr1@163.com>
2019-08-26 15:13:46 +0800

    Bagging Problems by houmo_XPS

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Bagging.h"
+
+int main() {
+    Bagging bagging;
+//    bagging.run();
+//    bagging.runFull();
+//    bagging.runComplete();
+    bagging.runMultiple();
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by houmo on 7/27/19.
+//
+
+#ifndef DP_MAIN_H
+#define DP_MAIN_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+#include "Bagging.h"
+
+#endif //DP_MAIN_H
